cc2be24fbb26b23c4b7e023c5642037aa798ac72
byron1st <hwi.ahn@gmail.com>
2014-05-15 16:21:51 +0900

    Add additional Classes for New Version Generator
    Add additional Classes for New Version Generator

---
+++ src/edu/kaist/g4/function/architectureVersionManagement/ArchitectureVersionManager.java
@@ -0,0 +1,7 @@
+package edu.kaist.g4.function.architectureVersionManagement;
+
+public class ArchitectureVersionManager {
+    public void commitNewArchitecture(String filePathforNewArchitecture) {
+        //TODO: 여기다 Sequence 다이어그램 내용 넣기
+    }
+}
