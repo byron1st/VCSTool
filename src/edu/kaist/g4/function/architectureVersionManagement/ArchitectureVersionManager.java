01b677f0764630cba83e16ce7b45ebfd3b6589ad
byron1st <hwi.ahn@gmail.com>
2014-05-15 16:27:14 +0900

    Add comments blocks
    Add comments block for each file that I'm implementing now

--- src/edu/kaist/g4/function/architectureVersionManagement/ArchitectureVersionManager.java
+++ src/edu/kaist/g4/function/architectureVersionManagement/ArchitectureVersionManager.java
@@ -1,5 +1,14 @@
 package edu.kaist.g4.function.architectureVersionManagement;
 
+/**
+ * 
+ * @FileName : ArchitectureVersionManager.java
+ * @Package  : edu.kaist.g4.function.architectureVersionManagement
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   :
+ * 
+ */
 public class ArchitectureVersionManager {
     public void commitNewArchitecture(String filePathforNewArchitecture) {
         //TODO: 여기다 Sequence 다이어그램 내용 넣기
--- src/edu/kaist/g4/function/architectureVersionManagement/NewVersionGenerator.java
+++ src/edu/kaist/g4/function/architectureVersionManagement/NewVersionGenerator.java
@@ -2,6 +2,15 @@ package edu.kaist.g4.function.architectureVersionManagement;
 
 import edu.kaist.g4.data.Architecture;
 
+/**
+ * 
+ * @FileName : NewVersionGenerator.java
+ * @Package  : edu.kaist.g4.function.architectureVersionManagement
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   :
+ * 
+ */
 public class NewVersionGenerator implements INewVersionGenerator {
     
     
