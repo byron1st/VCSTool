bd4818da646f7c43fe097386f0d709e9699f7bba
psjung <psjung@kaist.ac.kr>
2014-05-16 13:46:15 +0900

    Writer클래스가 아니라 Reader클래스였음. Writer 클래스 소스를 Reader 클래스 소스로 이동

--- src/edu/kaist/g4/function/fileManager/Reader.java
+++ src/edu/kaist/g4/function/fileManager/Reader.java
@@ -1,5 +1,25 @@
 package edu.kaist.g4.function.fileManager;
 
-public class Reader {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+public class Reader extends DefaultHandler{
+    Architecture_XML architecture;
+    XMLParsingRules parsingRule;
+    
+    public Architecture_XML getParsedData(){
+        return architecture;
+    }
+    
+    @Override  
+    public void startDocument() throws SAXException {  
+        architecture = new Architecture_XML();
+        parsingRule = new XMLParsingRules();
+    }  
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        parsingRule.executeRule(architecture, qName, attributes);
+    }
 }
