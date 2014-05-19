d867b933ef25e9ac1da5bbe49a385c51052145f7
byron1st <hwi.ahn@gmail.com>
2014-05-19 10:20:43 +0900

    FileManager랑 IFileManager 수정 (for 필수, by 휘)
    IFileManager에 내가 필요한 2개의 함수를 넣어둠.

--- src/edu/kaist/g4/function/fileManager/FileManager.java
+++ src/edu/kaist/g4/function/fileManager/FileManager.java
@@ -1,5 +1,21 @@
 package edu.kaist.g4.function.fileManager;
 
-public class FileManager {
+import edu.kaist.g4.data.Architecture;
+
+public class FileManager implements IFileManager{
+    
+    // TODO: Singleton 으로 할 필요가 있을까? Github 이슈 게시판 참조.
+
+    @Override
+    public Architecture readWorkingArchitecture(String filePath) {
+        // TODO 만들어줘!! by 휘. ~(-_ -)~
+        return null;
+    }
+
+    @Override
+    public Architecture readRecentArchitecture() {
+        // TODO 만들어줘!! by 휘. ~(-_ -)~
+        return null;
+    }
 
 }
--- src/edu/kaist/g4/function/fileManager/IFileManager.java
+++ src/edu/kaist/g4/function/fileManager/IFileManager.java
@@ -1,5 +1,23 @@
 package edu.kaist.g4.function.fileManager;
 
-public interface IFileManager {
+import edu.kaist.g4.data.Architecture;
 
+public interface IFileManager {
+    /**
+     * @Method Name : readWorkingArchitecture
+     * @Detail      : 유저가 commitNewArchitecture를 하면, filePath를 받아와서 Architecture를 읽어오는 함수.
+     *
+     * @param filePath
+     * @return
+     */
+    public Architecture readWorkingArchitecture(String filePath);
+    
+    /**
+     * @Method Name : readRecentArchitecture
+     * @Detail      : 시스템에 이미 저장되어 있던 가장 최근 Architecture를 불러오는 함수. 
+     *                Path는 이미 시스템에서 알고 있을 것이기 때문에 따로 변수로 넘기진 않음. 
+     *
+     * @return
+     */
+    public Architecture readRecentArchitecture();
 }
