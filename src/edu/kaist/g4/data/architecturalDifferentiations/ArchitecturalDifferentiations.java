d3a93fdcc4ffbe3127d6fc3e8b20cb77f66ce473
byron1st <hwi.ahn@gmail.com>
2014-05-19 10:40:34 +0900

    Architectural Differentiations 관련 클래스들 생성
    data 패키지 내에 architecturalDifferentiations 패키지를 생성하여 넣어주었음. Architecture
    구조와 동일하게 IArchitecturalDifferentiations를 통해 소통함.

---
+++ src/edu/kaist/g4/data/architecturalDifferentiations/ArchitecturalDifferentiations.java
@@ -0,0 +1,14 @@
+package edu.kaist.g4.data.architecturalDifferentiations;
+
+/**
+ * 
+ * @FileName : ArchitecturalDifferentiations.java
+ * @Package  : edu.kaist.g4.data.architecturalDifferentiations
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   : Architecture Change들과 Architecture Change Decision들의 리스트를 갖고 있는 클래스 
+ * 
+ */
+public class ArchitecturalDifferentiations implements IArchitecturalDifferentiations {
+
+}
---
+++ src/edu/kaist/g4/data/architecturalDifferentiations/ArchitectureChange.java
@@ -0,0 +1,14 @@
+package edu.kaist.g4.data.architecturalDifferentiations;
+
+/**
+ * 
+ * @FileName : ArchitectureChange.java
+ * @Package  : edu.kaist.g4.data
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   : 1개의 Architecture Change를 기록하는 클래스.
+ * 
+ */
+public class ArchitectureChange {
+
+}
