705477a3739cbcda16bfdc646ff8a046ea1a9a7d
byron1st <hwi.ahn@gmail.com>
2014-05-19 10:41:51 +0900

    Architectural Differentiations 패키지에 주석추가
    주석추가

--- src/edu/kaist/g4/data/architecturalDifferentiations/ArchitectureChangeDecision.java
+++ src/edu/kaist/g4/data/architecturalDifferentiations/ArchitectureChangeDecision.java
@@ -1,5 +1,14 @@
 package edu.kaist.g4.data.architecturalDifferentiations;
 
+/**
+ * 
+ * @FileName : ArchitectureChangeDecision.java
+ * @Package  : edu.kaist.g4.data.architecturalDifferentiations
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   : 1개의 Architecture Change Decision을 표현하는 클래스.
+ * 
+ */
 public class ArchitectureChangeDecision {
 
 }
--- src/edu/kaist/g4/data/architecturalDifferentiations/IArchitecturalDifferentiations.java
+++ src/edu/kaist/g4/data/architecturalDifferentiations/IArchitecturalDifferentiations.java
@@ -1,5 +1,14 @@
 package edu.kaist.g4.data.architecturalDifferentiations;
 
+/**
+ * 
+ * @FileName : IArchitecturalDifferentiations.java
+ * @Package  : edu.kaist.g4.data.architecturalDifferentiations
+ * @Author   : Hwi Ahn (ahnhwi@kaist.ac.kr)
+ * @Date     : 2014
+ * @Detail   : Architectural Differentiations의 내용을 호출하기 위한 interface.
+ * 
+ */
 public interface IArchitecturalDifferentiations {
 
 }
